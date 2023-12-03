924afc0f1bd71cdacde077b2277203157c8fc327
Abdul Wahab Amir <83786802+A-Wahab-Aamir@users.noreply.github.com>
2023-12-04 01:08:14 +0500

    Updated Work (filter Students)

---
+++ student.js
@@ -0,0 +1,51 @@
+var data=[
+    {
+        "Name":"Ahmed Ali",
+        "f_Name":"Yaqoob Ali",
+        "class":"VB",
+        "contact":"0311-823674",
+        "profile":"student_images/s1.jpg"
+    },
+    {
+        "Name":"Yasir",
+        "f_Name":"Mahmood",
+        "class":"VB",
+        "contact":"0311-823674",
+        "profile":"student_images/s2.jpg"
+    },
+    {
+        "Name":"Ali Khan",
+        "f_Name":"Tameez khan",
+        "class":"VB",
+        "contact":"0311-823674",
+        "profile":"student_images/s3.jpg"
+    },
+    {
+        "Name":"Maria Khan",
+        "f_Name":"Bilal Khan",
+        "class":"VB",
+        "contact":"0311-823674",
+        "profile":"student_images/s4.jpg"
+    },
+    {
+        "Name":"Farooq Hadiyat",
+        "f_Name":"Hadiayat",
+        "class":"VB",
+        "contact":"0311-823674",
+        "profile":"student_images/s5.jpg"
+    },
+    {
+        "Name":"Maroof Ali",
+        "f_Name":"khan Ali",
+        "class":"VB",
+        "contact":"0311-823674",
+        "profile":"student_images/s5.jpg"
+    },
+    {
+        "Name":"Khan zada",
+        "f_Name":"khan Ali",
+        "class":"ViB",
+        "contact":"0311-823674",
+        "profile":"student_images/s2.jpg"
+    },
+]
